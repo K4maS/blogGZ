9077ee1a42df6d3b2ecbe85afee8ce96024529ba
Kamil <107948849+K4maS@users.noreply.github.com>
2024-11-19 17:17:36 +0500

    Модифицировал меню для экранного помочника, так же дополнил другие части

--- src/index.js
+++ src/index.js
@@ -15,14 +15,37 @@ function doShowArticles() {
 function doBurgerMenu () {
     const burgerBtn = document.getElementById('burger');
     const menu = document.getElementById('mob-menu');
+    menu.ariaHidden="true";
 
-    burgerBtn.addEventListener('click', ()=> {
-        burgerBtn.classList.toggle('burger--active');
+    burgerBtn.addEventListener('click', (e)=> {
+        e.stopPropagation();
+        e.currentTarget.classList.toggle('burger--active');
+        
         menu.classList.toggle('nav--mobile');
+        
+        const menuIsOpen = menu.classList.contains('nav--mobile');
+        
+        menu.ariaHidden=!menuIsOpen;
+        burgerBtn.ariaExpanded=menuIsOpen;
+     
     })
+    
+        window.addEventListener('click', (e)=> {
+            if(e.target !== menu && e.target !== burgerBtn && menu.classList.contains('nav--mobile')) {
+                burgerBtn.classList.remove('burger--active');
+                menu.classList.remove('nav--mobile');
+            }
+        })
+    
+
 }
 
+if (window.innerWidth < 768) {
+    doBurgerMenu();
+} else if (window.innerWidth < 1200) {
+    console.log("Планшет");
+} else {
+    console.log("Десктоп");
+}
 
-
-doBurgerMenu ();
 doShowArticles();
