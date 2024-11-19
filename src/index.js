b009c00b7dcb2afe655995466004dbd68416847b
Kamil <107948849+K4maS@users.noreply.github.com>
2024-11-19 15:27:43 +0500

    Добавлены функции показа всех статей и бургер мнею

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './styles/styles.scss';
 
+function doShowArticles() {
+    const showArticlesBtn = document.getElementById('show-more-articles');
+    const articles = document.querySelectorAll('.article');
+
+    const showArticles = () => {
+        articles.forEach(elem => elem.style.display = 'block');
+    }
+
+    showArticlesBtn.addEventListener('click', showArticles);
+}
+
+
+function doBurgerMenu () {
+    const burgerBtn = document.getElementById('burger');
+    const menu = document.getElementById('mob-menu');
+
+    burgerBtn.addEventListener('click', ()=> {
+        burgerBtn.classList.toggle('burger--active');
+        menu.classList.toggle('nav--mobile');
+    })
+}
+
+
+
+doBurgerMenu ();
+doShowArticles();
